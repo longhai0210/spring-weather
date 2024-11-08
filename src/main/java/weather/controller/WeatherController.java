c0849db1768c6c74786c3ee4a5c614469c06207e
haitl <haitl@gisx.vn>
2024-11-08 22:15:12 +0700

    refactor weather mvc

--- src/main/java/weather/controller/WeatherController.java
+++ src/main/java/weather/controller/WeatherController.java
@@ -1,6 +1,5 @@
 package weather.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -17,16 +16,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 @Controller
-//@RequestMapping("/v1")
 public class WeatherController {
 
     @Autowired
     private Http http;
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Value("${weather.api.key}")
     private String apiKey;
@@ -39,29 +36,30 @@ public class WeatherController {
     @GetMapping("/weather")
     public String getWeather(@RequestParam(defaultValue = "") String location, Model model) {
         model.addAttribute("location", location);
+
         if(location.isEmpty()) {
             return "weather";
         }
         try{
             HttpResponse<String> res= http.get("https://api.openweathermap.org/data/2.5/weather?q="+ URLEncoder.encode(location, StandardCharsets.UTF_8) +"&units=metric&lang=vi&appid=" + apiKey);
-            Map<String, Object> data = objectMapper.readValue(res.body(), Map.class);
+            JSONObject data = new JSONObject(res.body());
 
             model.addAttribute("cod", res.statusCode());
 
             if(res.statusCode() == 200) {
-                Map<String, Object> dataMain = (Map)data.get("main");
-                List<Map<String, Object>> weather = (List<Map<String, Object>>)data.get("weather");
-                Map<String, Object> wind = (Map)data.get("wind");
-                model.addAttribute("name", data.get("name"));
-                model.addAttribute("time", DateTimeHelper.convertToLocalTime((int)data.get("dt"), (int)data.get("timezone")));
-                model.addAttribute("temp", Math.round((double)dataMain.get("temp")));
-                model.addAttribute("description", weather.get(0).get("description"));
-                model.addAttribute("icon", weather.get(0).get("icon"));
-                model.addAttribute("humidity", dataMain.get("humidity"));
-                model.addAttribute("windSpeed", wind.get("speed"));
+                JSONObject dataMain = data.getJSONObject("main");
+                JSONArray weather = data.getJSONArray("weather");
+                JSONObject wind = data.getJSONObject("wind");
+                model.addAttribute("name", data.getString("name"));
+                model.addAttribute("time", DateTimeHelper.convertToLocalTime(data.getInt("dt"), data.getInt("timezone")));
+                model.addAttribute("temp", Math.round(dataMain.getDouble("temp")));
+                model.addAttribute("description", weather.getJSONObject(0).getString("description"));
+                model.addAttribute("icon", weather.getJSONObject(0).getString("icon"));
+                model.addAttribute("humidity", dataMain.getNumber("humidity"));
+                model.addAttribute("windSpeed", wind.getNumber("speed"));
             }
             else {
-                model.addAttribute("message", data.get("message"));
+                model.addAttribute("message", data.getString("message"));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -78,30 +76,31 @@ public class WeatherController {
             return "daily";
         }
         try{
-            Map<String, List<Map<String, Object>>> dailyData = new HashMap<>();
+            Map<String, List<JSONObject>> dailyData = new HashMap<>();
             String selectedDate = "";
 
             HttpResponse<String> res= http.get("https://api.openweathermap.org/data/2.5/forecast?q="+ URLEncoder.encode(location, StandardCharsets.UTF_8) +"&units=metric&lang=vi&appid=" + apiKey);
-            Map<String, Map<String, Object>> data = objectMapper.readValue(res.body(), Map.class);
-
+//            Map<String, Map<String, Object>> data = objectMapper.readValue(res.body(), Map.class);
+            JSONObject data = new JSONObject(res.body());
             model.addAttribute("cod", res.statusCode());
 
             if(res.statusCode() == 200) {
-                List<Map<String, Object>> listWeather = (List<Map<String, Object>>)data.get("list");
+                JSONArray listWeather = data.getJSONArray("list");
                 List<Map<String, Object>> avgWeatherList = new ArrayList<>();
-                for (Map<String, Object> object : listWeather) {
-                    String day = ((String) object.get("dt_txt")).split(" ")[0];
+                for (int i =0; i < listWeather.length(); i++) {
+                    JSONObject object = listWeather.getJSONObject(i);
+                    String day = object.getString("dt_txt").split(" ")[0];
                     if(dailyData.get(day) != null) {
                         dailyData.get(day).add(object);
                     }
                     else {
-                        List<Map<String, Object>> emptyList = new ArrayList<>();
+                        List<JSONObject> emptyList = new ArrayList<>();
                         emptyList.add(object);
                         dailyData.put(day, emptyList);
                     }
                 }
 
-                for (Map.Entry<String, List<Map<String, Object>>> entry : dailyData.entrySet()) {
+                for (Map.Entry<String, List<JSONObject>> entry : dailyData.entrySet()) {
                     avgWeatherList.add(WeatherHelper.avgWeather(entry.getValue()));
                 }
                 avgWeatherList.sort((m1, m2) -> {
--- src/main/java/weather/utils/WeatherHelper.java
+++ src/main/java/weather/utils/WeatherHelper.java
@@ -1,9 +1,11 @@
 package weather.utils;
 
+import org.json.JSONObject;
+
 import java.util.*;
 
 public class WeatherHelper {
-    public static Map<String, Object> avgWeather(List<Map<String, Object>> weatherData) {
+    public static Map<String, Object> avgWeather(List<JSONObject> weatherData) {
         Map<String, Object> result = new HashMap<>();
         Map<String, Integer> mainWeather = new HashMap<>();
 
@@ -11,36 +13,36 @@ public class WeatherHelper {
         result.put("humidity", 0.0);
         result.put("temp_max", 0.0);
         result.put("temp_min", 9999.0);
-        result.put("format_time", DateTimeHelper.formatTime(weatherData.get(0).get("dt_txt").toString().split(" ")[0] + " 00:00:00", "EEE, d MMM"));
-        result.put("date", weatherData.get(0).get("dt_txt").toString().split(" ")[0]);
+        result.put("format_time", DateTimeHelper.formatTime(weatherData.get(0).getString("dt_txt").split(" ")[0] + " 00:00:00", "EEE, d MMM"));
+        result.put("date", weatherData.get(0).getString("dt_txt").split(" ")[0]);
 
-        for (Map<String, Object> weather : weatherData) {
-            if (((Map<String, Object>) weather.get("main")).get("temp") instanceof Integer){
-                result.put("temp", ((Double) result.get("temp")) + ((Map<String, Integer>) weather.get("main")).get("temp")) ;
+        for (JSONObject weather : weatherData) {
+            if (weather.getJSONObject("main").get("temp") instanceof Integer){
+                result.put("temp", (Double)result.get("temp") + weather.getJSONObject("main").getInt("temp")) ;
             }
             else {
-                result.put("temp", ((Double) result.get("temp")) + (Double)((Map<String, Object>) weather.get("main")).get("temp")) ;
+                result.put("temp", (Double) result.get("temp") + weather.getJSONObject("main").getDouble("temp")) ;
             }
-            if(((Map<String, Object>) weather.get("main")).get("temp_max") instanceof Double){
-                result.put("temp_max", Math.max((Double) result.get("temp_max"), ((Map<String, Double>) weather.get("main")).get("temp_max")));
-            }
-            else {
-                result.put("temp_max", Math.max((Double) result.get("temp_max"), ((Map<String, Integer>) weather.get("main")).get("temp_max")));
-            }
-            if(((Map<String, Object>) weather.get("main")).get("temp_min") instanceof Double){
-                result.put("temp_min", Math.min((Double) result.get("temp_min"), ((Map<String, Double>) weather.get("main")).get("temp_min")));
+            if(weather.getJSONObject("main").get("temp_max") instanceof Double){
+                result.put("temp_max", Math.max((Double) result.get("temp_max"), weather.getJSONObject("main").getDouble("temp_max")));
             }
             else {
-                result.put("temp_min", Math.min((Double) result.get("temp_min"), ((Map<String, Integer>) weather.get("main")).get("temp_min")));
+                result.put("temp_max", Math.max((Double) result.get("temp_max"), weather.getJSONObject("main").getInt("temp_max")));
             }
-            if(((Map<String, Integer>) weather.get("main")).get("humidity") instanceof Integer){
-                result.put("humidity", ((Double) result.get("humidity")) + ((Map<String, Integer>) weather.get("main")).get("humidity"));
+            if(weather.getJSONObject("main").get("temp_min") instanceof Double){
+                result.put("temp_min", Math.min((Double) result.get("temp_min"), weather.getJSONObject("main").getDouble("temp_min")));
             }
             else {
-                result.put("humidity", ((Double) result.get("humidity")) + ((Map<String, Integer>) weather.get("main")).get("humidity"));
+                result.put("temp_min", Math.min((Double) result.get("temp_min"), weather.getJSONObject("main").getInt("temp_min")));
+            }
+            if(weather.getJSONObject("main").get("humidity") instanceof Integer){
+                result.put("humidity", ((Double) result.get("humidity")) + weather.getJSONObject("main").getInt("humidity"));
+            }
+            else {
+                result.put("humidity", ((Double) result.get("humidity")) +  weather.getJSONObject("main").getDouble("humidity"));
             }
 
-            String wt = ((List<Map<String, String>>) weather.get("weather")).get(0).get("main");
+            String wt = weather.getJSONArray("weather").getJSONObject(0).getString("main");
             if(mainWeather.get(wt) == null) {
                 mainWeather.put(wt, 1);
             }
@@ -50,27 +52,27 @@ public class WeatherHelper {
         }
         String mainWt = Collections.max(mainWeather.entrySet(), Map.Entry.comparingByValue()).getKey();
         result.put("weather", mainWt);
-        result.put("temp", Math.round(((double) result.get("temp")) / weatherData.size()));
-        result.put("humidity", Math.round((double) result.get("humidity")) / weatherData.size());
+        result.put("temp", Math.round((double) result.get("temp") / weatherData.size()));
+        result.put("humidity", Math.round((double) result.get("humidity") / weatherData.size()));
         result.put("temp_max", Math.round((double) result.get("temp_max")));
         result.put("temp_min", Math.round((double) result.get("temp_min")));
         return result;
     }
 
-    public static List<Map<String, Object>> getWeatherDetail(List<Map<String, Object>> weatherData) {
+    public static List<Map<String, Object>> getWeatherDetail(List<JSONObject> weatherData) {
         List<Map<String, Object>> result = new ArrayList<>();
 
-        for (Map<String, Object> weather : weatherData) {
+        for (JSONObject weather : weatherData) {
             Map<String, Object> weatherDetail = new HashMap<>();
-            if(((Map<String, Double>) weather.get("main")).get("temp") instanceof Double){
-                weatherDetail.put("temp", Math.round(((Map<String, Double>) weather.get("main")).get("temp")));
+            if(weather.getJSONObject("main").get("temp") instanceof Double){
+                weatherDetail.put("temp", Math.round( weather.getJSONObject("main").getDouble("temp")));
             }
             else {
-                weatherDetail.put("temp", Math.round(((Map<String, Integer>) weather.get("main")).get("temp")));
+                weatherDetail.put("temp", Math.round(weather.getJSONObject("main").getDouble("temp")));
             }
-            weatherDetail.put("icon", ((List<Map<String, String>>) weather.get("weather")).get(0).get("icon"));
-            weatherDetail.put("time", DateTimeHelper.formatTime((String) weather.get("dt_txt"), "H:mm"));
-            weatherDetail.put("amPm", DateTimeHelper.formatTime((String) weather.get("dt_txt"), "a"));
+            weatherDetail.put("icon", weather.getJSONArray("weather").getJSONObject(0).getString("icon"));
+            weatherDetail.put("time", DateTimeHelper.formatTime(weather.getString("dt_txt"), "H:mm"));
+            weatherDetail.put("amPm", DateTimeHelper.formatTime(weather.getString("dt_txt"), "a"));
             result.add(weatherDetail);
         }
 
